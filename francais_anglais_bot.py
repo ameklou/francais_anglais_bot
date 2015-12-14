ee4c2a3edcc8be209b54b3298a5a24ce1503e7b1
METSOKO Daniel <metsoko.daniel@gmail.com>
2015-12-14 10:12:41 +0000

    suppression de l api de google et ajout de l api de yandex

--- francais_anglais_bot.py
+++ francais_anglais_bot.py
@@ -1,29 +1,71 @@
 #coding: utf-8
 
 from telegram import Updater
-import goslate
+from yandex_translate import YandexTranslate
 
 updater = Updater(token='112262702:AAHLgpm06rXHWtPYFJ_RwLPbw3M_RFwexcA')
 dispatcher = updater.dispatcher
+translate = YandexTranslate('trnsl.1.1.20151214T002932Z.b551b8b2ddb7845d.b1190d5048ddedd9ce60023e4a580d62dfa03704')
+
+
+def reformatter_la_traduction(original):
+	la_traduction_formater = original.replace(u'@Francais_Anglais_bot','')
+	la_traduction_formater = original.replace(u'/tofrench','')
+	la_traduction_formater = original.replace(u'/enchinois','')
+	la_traduction_formater = original.replace(u'/enhebreux','')
+	
+	if la_traduction_formater.replace(' ','') == '':
+		la_traduction_formater = u'Ajoutez moi du texte à traduire.'
+	
+	la_traduction_formater = u'Traduction: '+la_traduction_formater 
+	return la_traduction_formater
 
 def start(bot, update):
 	bot.sendMessage(chat_id=update.message.chat_id, text="Salut ! Je suis un traducteur, je peux vous donner la traduction anglaise d'un mot français ou d'une phrase.")
 
 def traduire(bot, update):
-    message_fr = update.message.text
-    bot_traducteur = goslate.Goslate()
+	global translate
+	message_toen = update.message.text
+	bot_traducteur = (translate.translate(message_toen, 'en'))
+	traduction_original = bot_traducteur[u'text'][0]
+	la_traduction = reformatter_la_traduction(traduction_original)
+	bot.sendMessage(chat_id=update.message.chat_id, text = la_traduction)
 
-    try:
-    	message_en = bot_traducteur.translate(message_fr, 'en')
-    except:
-    	message_en = u"Désolé j'ai pas pu traduire cette phrase"
+def tofrench(bot, update, args):
+	global translate
+	message_tofr = ' '.join(args)
+	bot_traducteur = (translate.translate(message_tofr, 'fr'))
+	traduction_original = bot_traducteur[u'text'][0]
+	la_traduction = reformatter_la_traduction(traduction_original)
+	bot.sendMessage(chat_id=update.message.chat_id, text = la_traduction)
 
-    bot.sendMessage(chat_id=update.message.chat_id, text = message_en)
+
+def enchinois(bot, update, args):
+	global translate
+	message_toch = ' '.join(args)
+	bot_traducteur = (translate.translate(message_toch, 'zh'))
+	traduction_original = bot_traducteur[u'text'][0]
+	la_traduction = reformatter_la_traduction(traduction_original)
+	bot.sendMessage(chat_id=update.message.chat_id, text = la_traduction)
+
+
+def enhebreux(bot, update, args):
+	global translate
+	message_tohe = ' '.join(args)
+	bot_traducteur = (translate.translate(message_tohe, 'he'))
+	traduction_original = bot_traducteur[u'text'][0]
+	la_traduction = reformatter_la_traduction(traduction_original)
+	bot.sendMessage(chat_id=update.message.chat_id, text = la_traduction)
 
 def unknown(bot, update):
 	bot.sendMessage(chat_id=update.message.chat_id, text= u"Désolé je ne comprend pas les commandes.")
 
+
 dispatcher.addTelegramCommandHandler('start', start)
+dispatcher.addTelegramCommandHandler('tofrench', tofrench)
+dispatcher.addTelegramCommandHandler('enchinois', enchinois)
+dispatcher.addTelegramCommandHandler('enhebreux', enhebreux)
+
 dispatcher.addUnknownTelegramCommandHandler(unknown)
 dispatcher.addTelegramMessageHandler(traduire)
 updater.start_polling()
